a655cf9c1f12e9e374a031fdc4d1fa6227458b74
Darianne Lopez <dariannelopez@dariannes-mbp.attlocal.net>
2019-03-18 20:54:11 -0700

    add legend

--- darianne_chart.js
+++ darianne_chart.js
@@ -1,4 +1,9 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
+function get_colors(n) {
+    var colors = ["#3977af","#b2c7e5", "#ef8536", "#f5bd82", "#519d3e"];
+    
+     return colors[ n % colors.length];}
+
+var margin = {top: 20, right: 20, bottom: 30, left: 70},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
@@ -6,10 +11,10 @@ var parseDate = d3.time.format("%x").parse;
     //formatPercent = d3.format(".0%");
 
 var x = d3.time.scale()
-    .range([0, width]);
+    .range([0, width*.85]);
 
 var y = d3.scale.linear()
-    .range([height, 0]);
+    .range([height*.85, 0]);
 
 var color = d3.scale.category20();
 
@@ -36,18 +41,18 @@ var svg = d3.select("body").select("#vis1").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    svg.append("text")
-    .attr("x", 0)
-    .attr("y", 50)
+    svg.append("text") //title
+    .attr("x", 105)
+    .attr("y", -20)
     .attr("dy", "0.71em")
     .attr("fill", "#000")
     .text("Top 5 Incident Types (December 2018 - February 2019)")
     .style("font", "23px avenir")
     .style("fill", "#000000");
 
-    svg.append("text")
+    svg.append("text") // bottom description
     .attr("x", 0)
-    .attr("y", 402)
+    .attr("y", 450)
     .attr("dy", "0em")
     .style("font", "12px avenir")
     .style("fill", "#000000")
@@ -68,7 +73,7 @@ d3.csv("call_type_data.csv", function(error, data) {
       })
     };
   }));
-console.log(browsers)
+//console.log(browsers)
   // Find the value of the day with highest total value
   var maxDateVal = d3.max(data, function(d){
     var vals = d3.keys(d).map(function(key){ return key !== "date" ? d[key] : 0 });
@@ -77,48 +82,76 @@ console.log(browsers)
 
   // Set domains for axes
   x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain([0, maxDateVal])
+  y.domain([-8, maxDateVal])
 
   var browser = svg.selectAll(".browser")
       .data(browsers)
-    .enter().append("g")
+      .enter().append("g")
       .attr("class", "browser");
 
   browser.append("path")
       .attr("class", "area")
       .attr("d", function(d) { 
-          console.log(d);
+          //console.log(d);
           return area(d.values); })
       .style("fill", function(d) { return color(d.name); });
 
-  browser.append("text")
-      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.y0 + d.value.y / 2) + ")"; })
-      .attr("x", -6)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name; });
+//   browser.append("text")
+//       .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+//       .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.y0 + d.value.y / 2) + ")"; })
+//       .attr("x", 5) //labels for each categorized area
+//       .attr("dy", ".35em")
+//       .text(function(d) { return d.name; });
 
-  svg.append("g")
+  svg.append("g") //x-axis
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
+      .attr("transform", "translate(0,+376.5)")
       .call(xAxis);
 
-    svg.append("text")
-      .attr("x", 350)
-      .attr("y", 0)
+    svg.append("text") //x-axis title
+      .attr("x", 290)
+      .attr("y", 420)
       .attr("fill", "#000")
       .text("Date of Incident")
       .style("font-weight", "bold");
 
-  svg.append("g")
+  svg.append("g") //y-axis
       .attr("class", "y axis")
       .call(yAxis)
-      .append("text")
+      .append("text") //y-axis title
       .attr("transform", "rotate(-90)")
-      .attr("x", -250)
-      .attr("y", -40)
+      .attr("x", -260)
+      .attr("y", -55)
       .attr("dy", "0.3408em")
       .attr("fill", "#000")
       .text("Number of Incidents")
       .style("font-weight", "bold"); 
+
+    // Creating legend
+    var legend = svg.selectAll(".legend")
+        .data(color.domain()).enter()
+        .append("g")
+        .attr("class","legend")
+        .attr("transform", "translate(" + (width-120) + "," + 10+ ")");
+    
+    // Creates color boxes in legend
+    legend.append("rect")
+        .attr("x", 0) 
+        .attr("y", function(d, i) { return 20 * i; })
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", function(d, i) {
+            return get_colors(i);}); 
+    
+    // Adds text to the legend
+    legend.append("text")
+        .attr("x", 20) 
+        .attr("dy", "0.75em")
+        .attr("y", function(d, i) { return 20 * i; })
+        .text(function(d) {return d});
+    
+    legend.append("text")
+        .attr("x",0) 
+        .attr("y",-13)
+        .text("Incident Call Type");
 });
